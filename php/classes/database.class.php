634f427f75d6e184c0e0f8c36dcbdb7c601286e0
Ernst Mulders <ernst.mulders@gmail.com>
2018-04-02 10:17:04 +0200

    Remove database password from the GIT 🤦🏼‍♂️

--- php/classes/database.class.php
+++ php/classes/database.class.php
@@ -11,7 +11,7 @@ class database{
 			$this->host = "127.0.0.1";
 			$this->databaseName = "IN4325"; 
 			$this->databaseUser = "IN4325";
-			$this->databasePassword = "4Qu9DaskfAEF6q2K";
+			$this->databasePassword = file_get_contents(__DIR__."/database.conf");
 			
 			try{
 				$this->handler = new PDO("mysql:host=".$this->host.";dbname=".$this->databaseName.";charset=utf8", $this->databaseUser, $this->databasePassword, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8mb4' COLLATE 'utf8mb4_unicode_ci'")); 
